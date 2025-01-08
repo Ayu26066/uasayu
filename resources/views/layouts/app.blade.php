84cdb1ff78151e6b70fec4971fc0ac802dc478a1
Trie Wahyu Ningsih <triewahyuningsih601@gmail.com>
2025-01-08 17:32:01 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,6 +60,9 @@
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('transactions.index') }}">Transaksi</a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('logout') }}">Logout</a>
+            </li>
         </ul>
     </div>
     <div class="content">
